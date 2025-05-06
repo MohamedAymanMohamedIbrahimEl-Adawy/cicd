61e1af6430fd05b91bcee6f2cb4bef330f19fc77
Mohamed Adawy <meladawi@2p.com.sa>
2025-05-06 15:01:46 +0300

    add print

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // print("Test un necessarily print");
+    print("Test un necessarily print");
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
